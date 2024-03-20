05599f4d086a09bde6c1d4c0b3b36af241ab1700
Pedro Oliveira <pedro120403@gmail.com>
2024-03-20 15:04:36 -0300

    Implementação dos aliens e mecanicas de jogo
    - Dinamica dos lasers
    - Disposição dos aliens na tela
    - Colisão dos lasers

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 import sys
+from random import choice
 
 import obstacle
 import pygame
+from alien import Alien
+from laser import Laser
 from player import Player
 
 
@@ -22,6 +25,12 @@ class Game:
         self.create_multiple_obstacles(
             *self.obstacle_x_positions, x_start=screen_width / 15, y_start=480)
 
+        # setup do alien
+        self.aliens = pygame.sprite.Group()
+        self.alien_laser = pygame.sprite.Group()
+        self.alien_setup(rows=6, cols=8)
+        self.alien_direction = 1
+
     def create_obstacle(self, x_start, y_start, offset_x):
         for row_index, row in enumerate(self.shape):
             for col_index, col in enumerate(row):
@@ -36,13 +45,81 @@ class Game:
         for x in offset:
             self.create_obstacle(x_start, y_start, x)
 
+    def alien_setup(self, rows, cols, x_distance=60, y_distance=48, x_offset=70, y_offset=100):
+        for row_index, row in enumerate(range(rows)):
+            for col_index, col in enumerate(range(cols)):
+                x = col_index * x_distance + x_offset
+                y = row_index * y_distance + y_offset
+
+                if row_index == 0:
+                    alien_sprite = Alien('yellow', x, y)
+                elif 1 <= row_index <= 2:
+                    alien_sprite = Alien('green', x, y)
+                else:
+                    alien_sprite = Alien('red', x, y)
+                self.aliens.add(alien_sprite)
+
+    def alien_position_checker(self):
+        all_aliens = self.aliens.sprites()
+        for alien in all_aliens:
+            if alien.rect.right >= screen_width:
+                self.alien_direction = -1
+                self.alien_move_down(2)
+            elif alien.rect.left <= 0:
+                self.alien_direction = 1
+                self.alien_move_down(2)
+
+    def alien_move_down(self, distance):
+        if self.aliens:
+            for alien in self.aliens.sprites():
+                alien.rect.y += distance
+
+    def alien_shoot(self):
+        if self.aliens.sprites():
+            random_alien = choice(self.aliens.sprites())
+            laser_sprite = Laser(random_alien.rect.center, 6, screen_height)
+            self.alien_laser.add(laser_sprite)
+
+    def collision_checks(self):
+        # laser do jogador
+        if self.player.sprite.lasers:
+            for laser in self.player.sprite.lasers:
+                # colisão com o obstaculo
+                if pygame.sprite.spritecollide(laser, self.blocks, True):
+                    laser.kill()
+
+                # colisão com o alien
+                if pygame.sprite.spritecollide(laser, self.aliens, True):
+                    laser.kill()
+
+                # colisão com o laser do alien
+                if pygame.sprite.spritecollide(laser, self.alien_laser, True):
+                    laser.kill()
+
+        if self.alien_laser:
+            for laser in self.alien_laser:
+                # colisão com o obstaculo
+                if pygame.sprite.spritecollide(laser, self.blocks, True):
+                    laser.kill()
+
+                # colisão com o alien
+                if pygame.sprite.spritecollide(laser, self.player, False):
+                    laser.kill()
+                    print('morreu')
+
     def run(self):
         self.player.update()
+        self.aliens.update(self.alien_direction)
+        self.alien_position_checker()
+        self.alien_laser.update()
+        self.collision_checks()
 
         self.player.sprite.lasers.draw(screen)
         self.player.draw(screen)
 
         self.blocks.draw(screen)
+        self.aliens.draw(screen)
+        self.alien_laser.draw(screen)
 
 
 if __name__ == '__main__':
@@ -53,11 +130,16 @@ if __name__ == '__main__':
     clock = pygame.time.Clock()
     game = Game()
 
+    ALIENLASER = pygame.USEREVENT + 1
+    pygame.time.set_timer(ALIENLASER, 800)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type == ALIENLASER:
+                game.alien_shoot()
         screen.fill((30, 30, 30))
         game.run()
 
